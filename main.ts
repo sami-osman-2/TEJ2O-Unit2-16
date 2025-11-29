28b9b4ddd2fed54025f74e038ff1e4dbf005c485
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-11-29 15:29:07 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
-*/
+radio.setGroup(7)
 
-basic.showString('Hello, World!')
+// distance
+function getDistance() {
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+
+    let duration = pins.pulseIn(DigitalPin.P9, PulseValue.High)
+    return duration / 58
+}
+
+basic.forever(function () {
+    // send
+    let d = getDistance()
+    if (d > 0 && d < 10) {
+        radio.sendString("Too Close")
+    }
+
+    // receive
+    let msg = radio.receiveString()
+    if (msg == "Too Close") {
+        basic.showString("Too Close")
+    }
+
+    basic.pause(100)
+})
