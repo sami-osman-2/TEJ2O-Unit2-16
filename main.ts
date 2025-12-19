178d9e21ab7d8cb8a3da432e32f0ec1d6895641e
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-12-19 12:40:40 -0500

    works

--- main.ts
+++ main.ts
@@ -1,31 +1,55 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
  * Created by: Sami Osman
- * Created on: Nov 2025
- * This program using a radio feature
+ * Created on: Oct 2025
+ * This program transmits a radio
 */
 
-// variables
-let distanceOfObstacle: number = 0
+let distance = 0
+let state = ""
 
-// cleanse
-radio.setGroup(39)
-basic.clearScreen()
-basic.showIcon(IconNames.Giraffe)
+radio.setGroup(247)
+radio.setTransmitPower(7)
+basic.showIcon(IconNames.Happy)
 
-// grabs distance
-while (true) {
-   distanceOfObstacle = sonar.ping(
-    DigitalPin.P12, 
-    DigitalPin.P13, 
-    PingUnit.Centimeters
+basic.forever(function () {
+    distance = sonar.ping(
+        DigitalPin.P12,
+        DigitalPin.P13,
+        PingUnit.Centimeters
     )
-    if (distanceOfObstacle < 10) {
-        radio.sendString('Too Close bub')
+
+    // Ignore bad readings
+    if (distance <= 0 || distance > 400) {
+        basic.pause(200)
+        return
     }
-    radio.onReceivedString(function(receivedString: string) {
-        basic.clearScreen()
-        basic.showString(receivedString)
-        basic.showIcon(IconNames.Happy)
-    })
-}
+
+    if (distance <= 5) {
+        if (state != "close") {
+            radio.sendString("Too close")
+            state = "close"
+        }
+    } else {
+        if (state != "safe") {
+            radio.sendString("Safe")
+            state = "safe"
+        }
+    }
+
+    basic.pause(300)
+})
+
+// this is recivere code
+radio.setGroup(247)
+basic.showIcon(IconNames.Heart)
+
+radio.onReceivedString(function (msg) {
+    basic.clearScreen()
+
+    if (msg == "Too close") {
+        basic.showIcon(IconNames.No)
+        basic.showString("WARNING")
+    } else if (msg == "Safe") {
+        basic.showIcon(IconNames.Yes)
+    }
+})
